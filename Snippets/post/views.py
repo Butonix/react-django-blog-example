26fd5ecea9f17e3583286abae7c59649a6e5ec28
Borislav Hadzhiev <opmftw@abv.bg>
2018-01-14 10:22:31 +0200

    added an offset possibility for the cursorpagination based on the kwarg the user has entered in his http request, have to fix the previous link for the cursor pagination

--- Snippets/post/views.py
+++ Snippets/post/views.py
@@ -13,7 +13,7 @@ from django.http import Http404
 class CustomCursorPagination(CursorPagination):
 	ordering= 'posted_on'
 	page_size=1
-	page_size_query_param='page_size'
+
 	def get_paginated_response(self, data):
 	    return Response({
 	        'links': {
@@ -23,15 +23,98 @@ class CustomCursorPagination(CursorPagination):
 	        'results': data[0]
 	    })
 
+	def _reverse_ordering(self,ordering_tuple):
+	    def invert(x):
+	        return x[1:] if x.startswith('-') else '-' + x
+	        print(tuple([invert(item) for item in ordering_tuple]))
+	    return tuple([invert(item) for item in ordering_tuple])
+
+	def paginate_queryset(self, queryset, request, view=None):
+		self.page_size = self.get_page_size(request)
+		if not self.page_size:
+			return None
+
+		self.base_url = request.build_absolute_uri()
+		self.ordering = self.get_ordering(request, queryset, view)
+
+		self.cursor = self.decode_cursor(request)
+		if self.cursor is None:
+			# Capture the URL keyword argument the user inserted
+			captured_slug = request.resolver_match.kwargs.get('slug')
+			# filter the queryset for an instance with a slug = the kwarg
+			starter_offset = queryset.filter(slug=captured_slug)
+			# set the offset to be the zeroth index of the filtering array.pk - 1
+			print(starter_offset[0].pk) 
+			(offset, reverse, current_position) = (starter_offset[0].pk-1, False, None)
+		else:
+			(offset, reverse, current_position) = self.cursor
+
+		if reverse:
+			queryset = queryset.order_by(self._reverse_ordering(self.ordering))
+		else:
+			queryset = queryset.order_by(*self.ordering)
+
+		if current_position is not None:
+			order = self.ordering[0]
+			is_reversed = order.startswith('-')
+			order_attr = order.lstrip('-')
+			
+			if self.cursor.reverse != is_reversed:
+				kwargs = {order_attr + '__lt': current_position}
+			else:
+				kwargs = {order_attr + '__gt': current_position}
+			queryset = queryset.filter(**kwargs)
+
+		results = list(queryset[offset:offset + self.page_size + 1])
+		self.page = list(results[:self.page_size])
+
+		if len(results) > len(self.page):
+			has_following_position = True
+			following_position = self._get_position_from_instance(results[-1], self.ordering)
+		else:
+			has_following_position = False
+			following_position = None
+		
+		if reverse:
+			self.page = list(reversed(self.page))
+		
+		if reverse:
+			self.has_next = (current_position is not None) or (offset > 0)
+			self.has_previous = has_following_position
+			if self.has_next:
+				self.next_position = current_position
+			if self.has_previous:
+				self.previous_position = following_position
+		else:
+			self.has_next = has_following_position
+			self.has_previous = (current_position is not None) or (offset > 0)
+			if self.has_next:
+				self.next_position = following_position
+			if self.has_previous:
+				self.previous_position = current_position
+
+		if (self.has_previous or self.has_next) and self.template is not None:
+			self.display_page_controls = True
+		
+		return self.page
+
+
+class CustomPageNumberPagination(PageNumberPagination):
+	page_size=1
+	page_size_query_param='page_size'
+
+
 class PostList(generics.ListAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer 
 
 class PostDetail(generics.ListAPIView):
-	queryset = Post.objects.all()
+	queryset=Post.objects.all()
 	serializer_class = PostSerializer 
 	lookup_field = 'slug'
 	pagination_class = CustomCursorPagination
+
+
 
 
 class PostListArchive(generics.ListAPIView):
