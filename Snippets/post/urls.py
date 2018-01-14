9f819582607bdd19c163658c267c404e52cbfef0
Borislav Hadzhiev <opmftw@abv.bg>
2018-01-14 14:30:59 +0200

    removed the CursorPagination as it doesn't suite a starting point other than index 0, added a url to access the posts via PK in addition to the slug option

--- Snippets/post/urls.py
+++ Snippets/post/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 	url(r"^$", views.PostList.as_view(), name='post-list'),
 	url(r"^posts/((?P<query>[-\w]+)/)?$", views.PostListFilter.as_view(), name='post-list-filter'),
 	url(r"^(?P<archive>[-0-9]{7})/$", views.PostListArchive.as_view(), name='post-list-archive'),
-	url(r"^(?P<slug>[-\w]+)/$",views.PostDetail.as_view() , name= "post-detail"),
+	url(r"^(?P<pk>[0-9]{1,6})/$",views.PostDetailPk.as_view() , name= "post-detail-pk"),
+	url(r"^(?P<slug>[-\w]+)/$",views.PostDetailSlug.as_view() , name= "post-detail-slug"),
 ]
