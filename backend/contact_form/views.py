417f2e64a046f357cb9be575c9e83fa42bddaa95
Borislav Hadzhiev <opmftw@abv.bg>
2018-01-27 22:06:17 +0200

    completed the captcha in the contactForm

--- backend/contact_form/views.py
+++ backend/contact_form/views.py
@@ -1,8 +1,31 @@
-from contact_form.models import ContactForm 
-from contact_form.serializers import ContactFormSerializer 
+from contact_form.models import ContactForm
+from contact_form.serializers import ContactFormSerializer
+from django.conf import settings
 
-from rest_framework import generics 
+import requests
+import json
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
 
 class ContactFormCreate(generics.ListCreateAPIView):
 	queryset = ContactForm.objects.all()
 	serializer_class = ContactFormSerializer
+
+	def create(self, request, *args, **kwargs):
+		g_recaptcha_response = request.data['g_recaptcha_response']
+		r = requests.post(settings.GR_CAPTCHA_URL, {
+		'secret': settings.GR_CAPTCHA_SECRET_KEY,
+		'response': g_recaptcha_response
+		})
+		if not json.loads(r.content.decode())['success']:
+			return Response(
+				{'captcha_error': 'Please complete the captcha.'},
+				status = status.HTTP_400_BAD_REQUEST
+			)
+
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		self.perform_create(serializer)
+		headers = self.get_success_headers(serializer.data)
+		return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
