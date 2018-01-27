014fc70e9041ef0c476b719118d02592b532114e
Borislav Hadzhiev <opmftw@abv.bg>
2018-01-27 19:09:57 +0200

    added an email address field to the registration form

--- frontend/src/components/authentication/Register.js
+++ frontend/src/components/authentication/Register.js
@@ -25,7 +25,7 @@ const styles = theme => ({
     marginLeft: "auto",
     marginRight: "auto",
     marginTop: "30px",
-    marginBottom: "15px",
+    marginBottom: "3em",
     width: "10%"
   }
 });
@@ -51,45 +51,61 @@ class MyInnerRegistrationForm extends Component {
         <form onSubmit={handleSubmit}>
           <TextField
             name="username"
-            label="Username"
             placeholder="Enter your Username"
             type="text"
             value={values.username}
             onChange={handleChange}
             onBlur={handleBlur}
-            className={classes.textField}
             error={errors.username && touched.username}
             helperText={errors.username && touched.username && errors.username}
+            label="Username"
+            className={classes.textField}
+            required
           />
-
+          <TextField
+            name="email"
+            placeholder="Enter your Email"
+            type="text"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={errors.email && touched.email}
+            helperText={errors.email && touched.email && errors.email}
+            label="Email Address"
+            className={classes.textField}
+            required
+          />
           <TextField
             name="password1"
-            label="Password"
             placeholder="Enter your Password"
             type="password"
             value={values.password1}
             onChange={handleChange}
             onBlur={handleBlur}
-            className={classes.textField}
             error={errors.password1 && touched.password1}
             helperText={
               errors.password1 && touched.password1 && errors.password1
             }
+            className={classes.textField}
+            label="Password"
+            required
           />
           <TextField
             name="password2"
-            label="Repeat your Password"
             placeholder="Repeat your password"
             type="password"
             value={values.password2}
             onChange={handleChange}
             onBlur={handleBlur}
-            className={classes.textField}
             error={errors.password2 && touched.password2}
             helperText={
               errors.password2 && touched.password2 && errors.password2
             }
+            className={classes.textField}
+            label="Repeat your Password"
+            required
           />
+
           <br />
           <Button
             raised
@@ -119,6 +135,7 @@ class MyInnerRegistrationForm extends Component {
 const EnhancedForm = withFormik({
   mapPropsToValues: () => ({
     username: "",
+    email: "",
     password1: "",
     password2: ""
   }),
@@ -129,20 +146,24 @@ const EnhancedForm = withFormik({
       .required("Password is required"),
     password2: Yup.string()
       .oneOf([Yup.ref("password1"), null], "Passwords don't match.")
-      .required("Password confirm is required")
+      .required("Password confirm is required"),
+    email: Yup.string()
+      .email("Invalid Email Address")
+      .required("Email is required")
   }),
   handleSubmit: (
-    { username, password1, password2 },
+    { username, email, password1, password2 },
     { props, setSubmitting, setErrors }
   ) => {
     console.log("submitting RegistrationF");
     props
-      .registerAction({ username, password1, password2 })
+      .registerAction({ username, email, password1, password2 })
       .then(resp => {
         if (
           resp.username ||
           resp.password1 ||
           resp.password2 ||
+          resp.email ||
           resp.non_field_errors
         ) {
           console.log(resp);
