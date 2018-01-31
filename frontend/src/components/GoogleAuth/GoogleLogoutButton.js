0f75ec86959dccf7d31d34c269b7388ce6047aa2
Borislav Hadzhiev <opmftw@abv.bg>
2018-01-31 08:30:49 +0200

    minor changes

--- frontend/src/components/GoogleAuth/GoogleLogoutButton.js
+++ frontend/src/components/GoogleAuth/GoogleLogoutButton.js
@@ -1,36 +1,36 @@
 import React, { Component } from "react";
 import { GoogleLogout } from "react-google-login";
 
-import gapi from "gapi-client";
-
 class GoogleLogoutButton extends Component {
   componentDidMount() {
-    gapi.load("auth2", () => {
-      this.auth2 = gapi.auth2.init({
+    window.gapi.load("auth2", () => {
+      this.auth2 = window.gapi.auth2.init({
         client_id:
-          "254472747355-6umtrkcedqn00tg7ec17l705ftttam0r.apps.googleusercontent.com"
+          "254472747355-6umtrkcedqn00tg7ec17l705ftttam0r.apps.googleusercontent.com",
+        cookiepolicy: "single_host_origin"
       });
     });
   }
-  signOut = dispatch => {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2
-      .signOut()
-      .then(() => {
-        console.log("User signed out.");
-        localStorage.removeItem("goog_avatar_url");
-        localStorage.removeItem("goog_name");
-        localStorage.removeItem("goog_email");
-      })
-      .then(() => this.props.googleLogoutAction())
-      .then(() => this.props.history.push("/"));
-  };
+
   render() {
+    const signOut = dispatch => {
+      var auth2 = window.gapi.auth2.getAuthInstance();
+      auth2
+        .signOut()
+        .then(() => {
+          console.log("User signed out.");
+          localStorage.removeItem("goog_avatar_url");
+          localStorage.removeItem("goog_name");
+          localStorage.removeItem("goog_email");
+        })
+        .then(() => this.props.googleLogoutAction())
+        .then(() => this.props.history.push("/"));
+    };
     return (
       <GoogleLogout
         buttonText="Logout"
-        onLogoutSuccess={this.signOut}
         className="loginBtn loginBtn--google"
+        onLogoutSuccess={signOut}
       />
     );
   }
