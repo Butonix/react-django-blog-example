192da562b23a5862863856fd0eb3db9d12a4d14e
Borislav Hadzhiev <opmftw@abv.bg>
2018-02-05 19:15:53 +0200

    cleanup

--- backend/authenticated_test_endpoint/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthenticatedTestEndpointConfig(AppConfig):
-    name = 'authenticated_test_endpoint'
--- backend/authenticated_test_endpoint/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from .views import DogList
-
-urlpatterns = [
-    url(r"^$", DogList.as_view())
-]
