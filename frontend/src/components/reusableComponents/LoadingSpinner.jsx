7f4ba396516a14a7dc46769d4e0d13f52862b5c1
Borislav Hadzhiev <opmftw@abv.bg>
2018-03-11 19:47:31 +0200

    minor changes

--- frontend/src/components/reusableComponents/LoadingSpinner.jsx
+++ frontend/src/components/reusableComponents/LoadingSpinner.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import LoadingImage from "./LoadingImage.gif";
 
 const LoadingSpinner = () => {
-	return (
-		<div className="row">
-			<div className="col-md-8">
-				<img
-					src="https://loading.io/spinners/stripe/index.svg"
-					alt="spinner here"
-					className="mx-auto d-block"
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="row">
+      <div className="col-md-8">
+        <img
+          src={LoadingImage}
+          alt="spinner here"
+          className="mx-auto d-block"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default LoadingSpinner;
