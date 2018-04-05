4cd2e325bc3c3e6ac4db6cef6b040e32ea3ee4d5
Borislav Hadzhiev <opmftw@abv.bg>
2018-04-06 00:18:59 +0300

    css changes

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -7,7 +7,7 @@ import scriptLoader from "react-async-script-loader";
 import { withStyles } from "material-ui/styles";
 import TextField from "material-ui/TextField";
 import Button from "material-ui/Button";
-
+import Paper from "material-ui/Paper";
 import { styles } from "./authentication/customStylesMui";
 
 class ContactForm extends Component {
@@ -81,127 +81,131 @@ class ContactForm extends Component {
     } = this.props;
 
     return (
-      <span className={classes.container}>
-        <h3 style={{ textAlign: "center" }}>Contact me</h3>
-        {this.props.contact_form.err ? (
-          <div className="alert alert-danger" role="alert">
-            <strong>{contact_form.err.message}</strong>
-          </div>
-        ) : null}
-        {this.props.contact_form.message ? (
-          <div className="alert alert-success" role="alert">
-            <strong>{contact_form.message}</strong>
-          </div>
-        ) : null}
-        <form onSubmit={e => this.handleSubmitAndCaptcha(e)}>
-          <TextField
-            name="first_name"
-            placeholder="Enter your First Name"
-            type="text"
-            value={values.first_name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={errors.first_name && touched.first_name}
-            helperText={
-              errors.first_name && touched.first_name && errors.first_name
-            }
-            label="First Name"
-            className={classes.textField}
-            required
-          />
+      <div className="container">
+        <span className={classes.container}>
+          <Paper elevation={5} style={{ padding: "3em" }}>
+            <h3 style={{ textAlign: "center" }}>Contact me</h3>
+            {this.props.contact_form.err ? (
+              <div className="alert alert-danger" role="alert">
+                <strong>{contact_form.err.message}</strong>
+              </div>
+            ) : null}
+            {this.props.contact_form.message ? (
+              <div className="alert alert-success" role="alert">
+                <strong>{contact_form.message}</strong>
+              </div>
+            ) : null}
+            <form onSubmit={e => this.handleSubmitAndCaptcha(e)}>
+              <TextField
+                name="first_name"
+                placeholder="Enter your First Name"
+                type="text"
+                value={values.first_name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.first_name && touched.first_name}
+                helperText={
+                  errors.first_name && touched.first_name && errors.first_name
+                }
+                label="First Name"
+                className={classes.textField}
+                required
+              />
 
-          <TextField
-            name="last_name"
-            placeholder="Enter your Last Name"
-            type="text"
-            value={values.last_name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={errors.last_name && touched.last_name}
-            helperText={
-              errors.last_name && touched.last_name && errors.last_name
-            }
-            label="Last Name"
-            className={classes.textField}
-            required
-          />
-          <TextField
-            name="email"
-            placeholder="Enter your Email"
-            type="text"
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={errors.email && touched.email}
-            helperText={errors.email && touched.email && errors.email}
-            label="Email Address"
-            className={classes.textField}
-            required
-          />
-          <TextField
-            name="website"
-            placeholder="Enter your Website"
-            type="url"
-            value={values.website}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={errors.website && touched.website}
-            helperText={errors.website && touched.website && errors.website}
-            label="Your Website"
-            className={classes.textField}
-          />
+              <TextField
+                name="last_name"
+                placeholder="Enter your Last Name"
+                type="text"
+                value={values.last_name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.last_name && touched.last_name}
+                helperText={
+                  errors.last_name && touched.last_name && errors.last_name
+                }
+                label="Last Name"
+                className={classes.textField}
+                required
+              />
+              <TextField
+                name="email"
+                placeholder="Enter your Email"
+                type="text"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.email && touched.email}
+                helperText={errors.email && touched.email && errors.email}
+                label="Email Address"
+                className={classes.textField}
+                required
+              />
+              <TextField
+                name="website"
+                placeholder="Enter your Website"
+                type="url"
+                value={values.website}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.website && touched.website}
+                helperText={errors.website && touched.website && errors.website}
+                label="Your Website"
+                className={classes.textField}
+              />
 
-          <TextField
-            name="message"
-            multiline={true}
-            rows={6}
-            value={values.message}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            error={errors.message && touched.message}
-            helperText={errors.message && touched.message && errors.message}
-            label="Your Message"
-            className={classes.textField}
-            required
-          />
-          <span
-            style={{
-              margin: "0 auto",
+              <TextField
+                name="message"
+                multiline={true}
+                rows={6}
+                value={values.message}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.message && touched.message}
+                helperText={errors.message && touched.message && errors.message}
+                label="Your Message"
+                className={classes.textField}
+                required
+              />
+              <span
+                style={{
+                  margin: "0 auto",
 
-              marginBottom: "1em",
-              textAlign: "center",
-              display: "block",
-              width: "304px"
-            }}
-          >
-            <Recaptcha
-              sitekey="6Le7xT4UAAAAAOuGdLd4TcqpXDRDZMIxvTn0CEYB"
-              render="explicit"
-              theme="dark"
-              onloadCallback={this.onloadCallback}
-              verifyCallback={this.verifyCallback}
-              ref={e => (this.recaptchaInstance = e)}
-            />
-          </span>
-          <Button
-            raised
-            className={classes.button}
-            type="button"
-            onClick={handleReset}
-            disabled={!dirty || isSubmitting}
-          >
-            Reset
-          </Button>
-          <Button
-            raised
-            className={classes.button}
-            type="submit"
-            disabled={isSubmitting}
-          >
-            Submit
-          </Button>
-        </form>
-      </span>
+                  marginBottom: "1em",
+                  textAlign: "center",
+                  display: "block",
+                  width: "304px"
+                }}
+              >
+                <Recaptcha
+                  sitekey="6Le7xT4UAAAAAOuGdLd4TcqpXDRDZMIxvTn0CEYB"
+                  render="explicit"
+                  theme="dark"
+                  onloadCallback={this.onloadCallback}
+                  verifyCallback={this.verifyCallback}
+                  ref={e => (this.recaptchaInstance = e)}
+                />
+              </span>
+              <Button
+                raised
+                className={classes.button}
+                type="button"
+                onClick={handleReset}
+                disabled={!dirty || isSubmitting}
+              >
+                Reset
+              </Button>
+              <Button
+                raised
+                className={classes.button}
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Submit
+              </Button>
+            </form>
+          </Paper>
+        </span>
+      </div>
     );
   }
 }
